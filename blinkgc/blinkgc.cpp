c830123375bc39e7767bfb5756e5614403537963
weolar <weolar@qq.com>
2018-04-23 21:56:41 +0800

    * 增加点测试例子

--- blinkgc/blinkgc.cpp
+++ blinkgc/blinkgc.cpp
@@ -24,11 +24,29 @@ WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
 namespace blink {
 
 class Element : public blink::GarbageCollectedFinalized<Element> {
+public:
     DECLARE_VIRTUAL_TRACE();
+
+    ~Element()
+    {
+        OutputDebugStringA("~Element\n");
+    }
 };
 
 DEFINE_TRACE(Element)
 {
+}
+
+class Ungc : public blink::GarbageCollectedFinalized<Ungc> {
+public:
+    DECLARE_VIRTUAL_TRACE();
+
+    ~Ungc() {
+        OutputDebugStringA("~Ungc\n");
+    }
+};
+
+DEFINE_TRACE(Ungc) {
 }
 
 class EventTarget : public blink::GarbageCollectedFinalized<EventTarget> {
@@ -40,7 +58,7 @@ public:
 
     ~EventTarget()
     {
-
+        OutputDebugStringA("~EventTarget\n");
     }
 
     DECLARE_VIRTUAL_TRACE();
@@ -54,6 +72,11 @@ DEFINE_TRACE(EventTarget)
 }
 
 }
+
+class TestPersistent {
+public:
+    blink::Persistent<blink::Ungc> m_ungc;
+};
 
 // 此代码模块中包含的函数的前向声明: 
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -92,13 +115,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     return (int) msg.wParam;
 }
 
-
-
-//
-//  函数: MyRegisterClass()
-//
-//  目的: 注册窗口类。
-//
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
     WNDCLASSEXW wcex;
@@ -120,16 +136,6 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     return RegisterClassExW(&wcex);
 }
 
-//
-//   函数: InitInstance(HINSTANCE, int)
-//
-//   目的: 保存实例句柄并创建主窗口
-//
-//   注释: 
-//
-//        在此函数中，我们在全局变量中保存实例句柄并
-//        创建和显示主程序窗口。
-//
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // 将实例句柄存储在全局变量中
@@ -147,16 +153,6 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    return TRUE;
 }
 
-//
-//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  目的:    处理主窗口的消息。
-//
-//  WM_COMMAND  - 处理应用程序菜单
-//  WM_PAINT    - 绘制主窗口
-//  WM_DESTROY  - 发送退出消息并返回
-//
-//
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
@@ -181,6 +177,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     case WM_PAINT:
         {
             new blink::EventTarget();
+            TestPersistent* testPersistent = new TestPersistent();
+            testPersistent->m_ungc = new blink::Ungc();
 
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
